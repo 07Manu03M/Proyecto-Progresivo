c1f92302ebbd7551e8d17b986f02730ae21a230b
Manuel Meneses <manuelmeneses7200@gmail.com>
2025-04-08 21:55:03 -0500

    feat: :lipstick: search bar
    Only the design of a search bar was introduced

--- javascript/menu.js
+++ javascript/menu.js
@@ -3,9 +3,11 @@ const barralateral = document.querySelector(".barra_lateral");
 const spans = document.querySelectorAll("span");
 const palanca = document.querySelector(".switch");
 const circulo = document.querySelector(".circulo");
+const header = document.querySelector("header");
 
 susuwatari.addEventListener("click",()=>{
     barralateral.classList.toggle("mini_barra_lateral");
+    header.classList.toggle("min_busqueda");
     
     spans.forEach((span)=>{
         span.classList.toggle("oculto");
