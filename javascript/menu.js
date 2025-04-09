3748b89f4639cfc597fd50e17ea0d1ff5e3839a9
manuel meneses <manuelmeneses7200@gmail.com>
2025-04-09 11:10:29 -0500

    feat:

--- javascript/menu.js
+++ javascript/menu.js
@@ -4,10 +4,12 @@ const spans = document.querySelectorAll("span");
 const palanca = document.querySelector(".switch");
 const circulo = document.querySelector(".circulo");
 const header = document.querySelector("header");
+const main = document.querySelector("main")
 
 susuwatari.addEventListener("click",()=>{
     barralateral.classList.toggle("mini_barra_lateral");
     header.classList.toggle("min_busqueda");
+    main.classList.toggle("min-main")
     
     spans.forEach((span)=>{
         span.classList.toggle("oculto");
